9049602d8d0628761aebdbbb26d336dc29a3eea5
grafke <grafke@users.noreply.github.com>
2017-01-16 14:58:03 +0100

    Update vw.py
    ..

--- vw.py
+++ vw.py
@@ -63,7 +63,7 @@ class VW():
         Audited feature format:   Namespace^featureName:142703:1:0.0435613@0.25
         :param result:
         """
-        f = [x.split('@')[0].split(self._delimiter) for x in result.strip().split('\t')]
+        f = [x.rsplit('@', 1)[0].split(self._delimiter) for x in result.strip().split('\t')]
         for x in f:
             self.__add_feature(x)
 
